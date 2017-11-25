a7292946c84e62413a144ae5788aa75856b01728
Matthew Smith <matthewalexander108@gmail.com>
2017-11-25 16:24:19 -0500

    Refactor db contraint specs to skip model validations

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Movie, type: :model do
       rescue
         something_else_threw_error = true
       end
+
       expect(!something_else_threw_error).to be(true), "There is an error in our test code"
       expect(database_threw_error && !something_else_threw_error).to be(true), error_message
     end
@@ -17,14 +18,14 @@ RSpec.describe Movie, type: :model do
     it "enforces name not null" do
       movie = Movie.new(:rating => 'PG', :length_minutes => '10')
       test_for_db_error("Database did not catch null name") do
-        movie.save!
+        movie.save(validate: false)
       end
     end
 
     it "enforces name not empty" do
       movie = Movie.new(:name => '', :rating => 'PG', :length_minutes => '10')
       test_for_db_error("Database did not catch empty name") do
-        movie.save!
+        movie.save(validate: false)
       end
     end
 
@@ -36,15 +37,15 @@ RSpec.describe Movie, type: :model do
       end
 
       test_for_db_error("Database did not catch duplicate movie") do
-        movies[0].save!
-        movies[1].save!
+        movies[0].save(validate: false)
+        movies[1].save(validate: false)
       end
     end
 
     it "enforces rating not null" do
       movie = Movie.new(:name => 'Casablanca', :length_minutes => '10')
       test_for_db_error("Database did not catch null rating") do
-        movie.save!
+        movie.save(validate: false)
       end
     end
 
@@ -52,14 +53,14 @@ RSpec.describe Movie, type: :model do
       movie = Movie.new(:name => 'Casablanca', :rating => 'Fred',
                         :length_minutes => '10')
       test_for_db_error("Database did not catch invalid rating") do
-        movie.save!
+        movie.save(validate: false)
       end
     end
 
     it "enforces length is not null" do
       movie = Movie.new(:name => 'Casablanca', :rating => 'PG')
       test_for_db_error("Database did not catch null movie length") do
-        movie.save!
+        movie.save(validate: false)
       end
     end
 
@@ -67,7 +68,7 @@ RSpec.describe Movie, type: :model do
       movie = Movie.new(:name => 'Casablanca', :rating => 'PG',
                         :length_minutes => '0')
       test_for_db_error("Database did not catch zero length movie") do
-        movie.save!
+        movie.save(validate: false)
       end
     end
 
@@ -75,7 +76,7 @@ RSpec.describe Movie, type: :model do
       movie = Movie.new(:name => 'Casablanca', :rating => 'PG',
                         :length_minutes => '-10')
       test_for_db_error("Database did not catch negative movie length") do
-        movie.save!
+        movie.save(validate: false)
       end
     end
   end
